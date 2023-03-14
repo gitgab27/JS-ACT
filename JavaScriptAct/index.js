a4d9f3d81acb05adb4420402de0639e73cd96d06
Gabriel M. Arevalo <124123948+gitgab27@users.noreply.github.com>
2023-03-14 12:30:29 +0800

    Add files via upload

---
+++ JavaScriptAct/index.js
@@ -0,0 +1,6 @@
+var totalpricedisplay = function() {
+var total_price = parseInt(document.getElementById("Magic").value) + parseInt(document.getElementById("metal").value);
+
+return alert("Total Price of Item" + total_price);
+
+}
